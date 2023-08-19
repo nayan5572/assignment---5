26bb0503abd08981dfb74db8b30b303c47ca6c8c
nayan <nayan.dev572@gmail.com>
2023-08-20 04:16:21 +0600

    work finish input field

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // this code is for total Price calculation
 let total = 0;
-
+// const totalPriceButton = document.getElementById('totalPriceButton');
 
 
 function productName(target) {
@@ -25,15 +25,47 @@ function productName(target) {
     document.getElementById('totalPrice').innerText = total;
 
 
-
     if (total > 0) {
         totalPriceButton.removeAttribute('disabled');
         totalPriceButton.classList.remove('bg-gray-400');
         totalPriceButton.classList.add('bg-[#E527B2]');
-    } else {
+    }
+    else {
         totalPriceButton.setAttribute('disabled', 'disabled');
         totalPriceButton.classList.remove('bg-[#E527B2]');
         totalPriceButton.classList.add('bg-gray-400');
     }
+
+
+    // condition for special coupon
+    // document.getElementById('couponId').addEventListener('click', function(){
+    //     const specialCouponField = document.getElementById('couponCode');
+    //     const specialField = specialCouponField.value;
+    //     if(specialField === 'SELL20' && total >= 200){
+    //         console.log('USA');
+    //     }
+    //     else{
+    //         console.log('BAN');
+    //     }
+    // });
+    toggleApplyButtonState();
 }
+
+
+var couponCodeField = document.getElementById('couponCode');
+var applyButton = document.getElementById('couponId');
+
+function toggleApplyButtonState(){
+    const couponCode = couponCodeField.value;
+    if(couponCode === 'SELL20' && total >= 200){
+        applyButton.removeAttribute('disabled');
+        applyButton.classList.remove('bg-gray-400');
+        applyButton.classList.add('bg-[#E527B2]');
+    }else{
+        applyButton.setAttribute('disabled', 'disabled');
+        applyButton.classList.remove('bg-[#E527B2]');
+        applyButton.classList.add('bg-gray-400');
+    }
+}
+couponCodeField.addEventListener('input', toggleApplyButtonState);
 
